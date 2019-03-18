34bb3aecb0f511e50d55b2ded4e8e4f4595c1fad
Ethan Brierley <ethanboxx@gmail.com>
2019-03-18 11:09:01 +0000

    use functions

--- lesson_1/l_1.2_first_prog.hs
+++ lesson_1/l_1.2_first_prog.hs
@@ -6,4 +6,12 @@ messyMain = do
     title <- getLine
     print "Who is the Author?"
     author <- getLine
-    print ("Dear " ++ recipient ++ ", \n" ++ "Thanks for buying " ++ title ++ "\nthanks, \n" ++ author)
+    print (createEmail recipient title author)
+
+toPart recipient = "Dear " ++ recipient ++ ",\n"
+
+bodyPart bookTitle = "Thanks for buying " ++ bookTitle ++ ".\n"
+
+fromPart author = "Thanks,\n " ++ author
+
+createEmail recipient bookTitle author = toPart recipient ++ bodyPart bookTitle ++ fromPart author
