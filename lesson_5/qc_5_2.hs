9e3f0dc0669168b901f4a799f9a1b4c45aca2422
Ethan Brierley <ethanboxx@gmail.com>
2019-04-01 12:14:05 +0100

    add

--- lesson_5/qc_5_2.hs
+++ lesson_5/qc_5_2.hs
@@ -1,3 +1,5 @@
 getRequestURL host apiKey resource id = host ++ "/" ++ resource ++ "/" ++ id ++ "?token" ++ apiKey
 
 genApiResourceRequestBuilder apiKey resouce = getRequestURL "http://example.com" apiKey resouce
+
+bookFromExmaple = genApiResourceRequestBuilder "1337hAsk311" "book"
