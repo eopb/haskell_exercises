0b7f8120dfe138e05c804b0f0985ee6afa05ae69
Ethan Brierley <ethanboxx@gmail.com>
2019-04-02 22:14:05 +0100

    update

--- bubble.hs
+++ bubble.hs
@@ -1,23 +1,22 @@
-swap list index = listBefore ++ swapedValues ++ listAfter
+bubbleSort f list = bubbleSort f list 0 0
     where
-        swapedValues = [list !! (index + 1), list !! index]
-        listBefore = take (index) list
-        listAfter = drop (index + 2) list
-
-bubbleSortGen f list position numberOfCorrects = if onLastItem
-    then if finished
-        then list
-        else skipTo 0 0
-    else if partAlreadySorted
-        then skipTo (position + 1) (numberOfCorrects + 1)
-        else nextSortIter (swap list position) (position + 1) numberOfCorrects
-    where
+    bubbleSort f list position numberOfCorrects = if onLastItem
+        then if finished
+            then list
+            else skipTo 0 0
+        else if partAlreadySorted
+            then skipTo (position + 1) (numberOfCorrects + 1)
+            else nextSortIter (swap list position) (position + 1) numberOfCorrects
+        where
         onLastItem = position + 1 == length list
         finished = numberOfCorrects == length list - 1
-        nextSortIter = bubbleSortGen f
+        nextSortIter = bubbleSort f
         skipTo = nextSortIter list
         partAlreadySorted = f (list !! position) (list !! (position + 1))
-
-bubbleSort f list = bubbleSortGen f list 0 0
+        swap list index = listBefore ++ swapedValues ++ listAfter
+            where
+            swapedValues = [list !! (index + 1), list !! index]
+            listBefore = take (index) list
+            listAfter = drop (index + 2) list
 
 bubbleSortInt list = bubbleSort (\x y -> x >= y) list
