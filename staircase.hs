0ea2c96bdff9d43e6a3482a2c95f214ed6533eda
Ethan Brierley <ethanboxx@gmail.com>
2019-04-11 07:32:07 +0100

    staircase

--- staircase.hs
+++ staircase.hs
@@ -1,12 +1,10 @@
 
 staircaseLine 0 _ = []
-staircaseLine width hashNum = 
+staircaseLine width hashNum =
     (if width <= hashNum
     then '#'
     else ' '):staircaseLine (width - 1) hashNum
-
-staircaseBuild width hashNum = if hashNum == 0 
-    then ""
-    else staircaseBuild width (hashNum - 1) ++ staircaseLine width hashNum ++ "\n"
+staircaseBuild width 0 = ""
+staircaseBuild width hashNum = staircaseBuild width (hashNum - 1) ++ staircaseLine width hashNum ++ "\n"
 
 staircase height = staircaseBuild height height
