ce73d4a29a589fcfef1b15befa103e96b2ea3e1b
Ethan Brierley <ethanboxx@gmail.com>
2019-05-01 14:02:32 +0100

    more code

--- lang.hs
+++ lang.hs
@@ -8,30 +8,32 @@ type Word = T.Text
 
 data Token = Word | ArrowLeft | ArrowRight | If deriving( Show )
 
-a :: T.Text
-
-a = "hello"
+tokenizeSentence :: T.Text -> [Token]
+tokenizeSentence sent = case nextToken of
+        Just If -> If:(tokenizeSentence (T.drop 3 sent))
+        Just ArrowLeft -> ArrowLeft:(tokenizeSentence (T.drop 3 sent))
+        Just ArrowRight-> ArrowRight:(tokenizeSentence (T.drop 3 sent))
+        Nothing -> []
+    where
+        nextToken = tokenizeWord sent
 
 tokenizeWord :: T.Text -> Maybe Token
-tokenizeWord x = if (T.head x == ' ') || (T.head x == '\n')
+tokenizeWord x = if (T.length x) == 0
+                      then Nothing
+                 else if (T.head x == ' ') || (T.head x == '\n')
                      then tokenizeWord (T.tail x)
                  else if isNextWord "if"
                      then Just If
+                 else if isNextWord "<-"
+                     then Just ArrowLeft
+                 else if isNextWord "->"
+                     then Just ArrowRight
                  else Nothing
-    where isNextWord = isNextWordOf x
-
-nextTokenIs :: T.Text -> Maybe Token
-nextTokenIs x = case (T.take 3 x) of
-    ("if ") -> Just If
-    _       -> Nothing
     where isNextWord = isNextWordOf x
 
 test = isNextWordOf "if afjkasd" "if"
 tesdt = tokenizeWord "if afjkasd"
 
-tryMaybe :: Maybe a -> Maybe a -> Maybe a
-tryMaybe (Just x) y = Just x
-tryMaybe Nothing  y = y
 
 isNextWordOf :: T.Text -> T.Text -> Bool
 isNextWordOf sentence word = (T.snoc word ' ') == firstLengthLetters
